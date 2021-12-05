e79f76385314bd09fe2946d30357a62e741d20c9
nnikhil <nikhil_narendran@hotmail.com>
2021-12-05 15:27:43 -0500

    arequired nod modules

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
+const http = require('http');
+const pdfKit = require('pdfkit');
+const axios = require('axios');
+
+const port = 3001;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at: ${port}/`);
+});
+
+
+
+
+
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
