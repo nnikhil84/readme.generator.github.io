54c4954bc7bc5311dd633d6483dd9e625d4386e6
nnikhil <nikhil_narendran@hotmail.com>
2021-12-07 21:47:51 -0500

    minor changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const util = require('util');
@@ -14,24 +14,91 @@ const server = http.createServer((req, res) => {
   res.end("Hello World");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at: ${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at: ${port}`);
 });
 
+// User quesions array
+const questions = 
+    [{
+        type: "input",
+        name: "ghubUsername",
+        message: "Please enter your GitHub Username"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your e-mail ID"
+    },
+    {
+        type: "input",
+        name: "repository",
+        message: "Please enter your GitHub repository"
+    },
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "Please enter the project title"
+    }
+];
 
+inquirer.prompt(questions).then((answers) => {
+  // call getGitHubProfileInfo function
+  console.log(JSON.stringify(answers, null, '  '));
+  getGitHubProfileInfo(
+    answers.ghubUsername,
+    answers.email,
+    answers.repository,
+    answers.projectTitle
+  );
+});
 
+// Function for retrieving user GitHub Profile information based on their responses
+async function getGitHubProfileInfo(user, email, repo, title) {
+  // Use axios to make an API call to retrieve the user's GitHub information
+  const { data } = await axios.get(`https://api.github.com/users/${user}`);
 
+  data.email = email;
 
+  // Generate a URL for their GitHub profile based on the response
+  const repoURL = `https://github.com/${user}/${repo}`;
 
+  console.log("Data: ", data);
+  console.log("Repo URL: ", repoURL);
+  console.log("Project Title: ", title);
 
-// TODO: Create an array of questions for user input
-const questions = [];
+  // Turn the results into a string
+  const stringData = JSON.stringify(data, null, "  ");
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  // Variable containing code for readme template to be generated
+  const result = `
+# Title: ${title} 
+## Project Description 
+## Contents
+## Tech used
+## License
+## Contributing
+## Tests
+## Questions
+## Issues
+* GitHub profile avatar:
+![alt text](${data.avatar_url} "User GitHub Profile Picture")
+* GitHub profile username: [${data.login}](${data.html_url})
+* GitHub email: [${data.email}](mailto:${data.email})
+* GitHub repository: ${repoURL}
+`;
 
-// TODO: Create a function to initialize app
-function init() {}
+  // Log results
+  console.log(result);
 
-// Function call to initialize app
-init();
+  // Write to a readme file
+  fs.writeFile("readmetemplate.md", result, function (err) {
+    if (err) return console.log(err);
+  });
+
+  console.log(data.avatar_url);
+
+  // Close server when complete
+  server.close();
+}
+
